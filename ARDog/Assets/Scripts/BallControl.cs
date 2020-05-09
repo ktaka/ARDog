44707d486d21d9a2f030089cf01b2289d0252b29
Kenichi Takahashi <kenichi.takahashi@kabuku.co.jp>
2020-05-09 20:58:48 +0900

    rename Ball Control script.

--- ARDog/Assets/Scripts/BallControl.cs
+++ ARDog/Assets/Scripts/BallControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallContorl : MonoBehaviour
+public class BallControl : MonoBehaviour
 {
     // オブジェクトを生成する際のテンプレートとなるプレハブ
     public GameObject ballObject;
@@ -10,7 +10,7 @@ public class BallContorl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
